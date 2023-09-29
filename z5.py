85c2cadd47f720b6f8fa5abb8ec6495f32a7e0b0
Posyolova <143779118+Posyolova@users.noreply.github.com>
2023-09-30 01:25:41 +0700

    first commint

---
+++ z5.py
@@ -0,0 +1,7 @@
+a  = int(input('введите a- ' ))
+b  = int(input('введите b- '))
+c = int(input('введите c- '))
+if a <b+c and b<a+c and c<a+b:
+        print('такой треугольник есть')
+else:
+        print('такого нет') 
