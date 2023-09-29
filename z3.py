bc8f9526565b9c03872c59fb2f3ab639a0c16f51
Posyolova <143779118+Posyolova@users.noreply.github.com>
2023-09-30 01:22:07 +0700

    first commit

---
+++ z3.py
@@ -0,0 +1,9 @@
+a = int(input('введите  а- '))
+b = int(input('введите  б- '))
+
+if a % b == 0:
+        print('делится')
+        print( a // b)
+else:
+        print('не делится')
+        print(a % b)
