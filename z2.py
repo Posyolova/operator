30fe67e9ac21f10fc7a77dbfc2d6766a8b8f01c2
Posyolova <143779118+Posyolova@users.noreply.github.com>
2023-09-30 01:20:46 +0700

    first commit

---
+++ z2.py
@@ -0,0 +1,12 @@
+a = int(input('введите a-  '))
+b = int(input('введите b-  '))
+c = int(input('введите c- '))
+
+if (a > b and a < c) or (a > c and a < b):
+        print(a)
+elif (b > a and b < c) or (b > c and b < a):
+        print(b)
+elif (c > a and c < b) or (c > b and c < a):
+        print(c)
+elif a == b or a == c or b == c:
+        print('error') 
