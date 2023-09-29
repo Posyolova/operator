58486ee8bc83c4305414509fed41f067d1292921
Posyolova <p0lyamorozova@yandex.ru>
2023-09-30 01:18:24 +0700

    first commit

---
+++ z1.py
@@ -0,0 +1,10 @@
+a = int(input('введите a- '))
+b = int(input('введите b- '))
+c = int(input('введите c- '))
+
+if a > b  and a > c:
+        print(a)
+elif b > a and b > c:
+        print(b)
+elif c > a and c > b:
+        print(c)
